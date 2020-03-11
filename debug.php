57f51f49e18f1450fd33efc75139d41f9c067d22
Ian Moffitt <IMoffitt@barkleyrei.com>
2020-03-11 09:07:57 -0400

    feat: Add more debugging.

--- debug.php
+++ debug.php
@@ -17,11 +17,18 @@
 	$api = _get_api_object();
 	$me = $api->me();
 	$devices = $api->getMyDevices();
+	$playing = $api->getMyCurrentPlaybackInfo();
+	$playing2 = $api->getMyCurrentTrack();
 
 	$current = current_track($cache, $breaker);
 
 	print_r($current);
 
+	print_r($playing);
+
+	print_r($playing2);
+
 	print_r($me);
 
 	print_r($devices);
+
