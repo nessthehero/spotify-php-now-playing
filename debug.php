387006f4797b52b0fb6dc06a828bcaeccbb25358
Ian Moffitt <IMoffitt@barkleyrei.com>
2020-03-11 09:11:38 -0400

    feat: Add more debugging.

--- debug.php
+++ debug.php
@@ -19,16 +19,31 @@
 	$devices = $api->getMyDevices();
 	$playing = $api->getMyCurrentPlaybackInfo();
 	$playing2 = $api->getMyCurrentTrack();
+	$recent = $api->getMyRecentTracks();
 
 	$current = current_track($cache, $breaker);
 
+	print_r('-- current --' . PHP_EOL);
 	print_r($current);
+	print_r('-- end current --' . PHP_EOL);
 
+	print_r('-- playing --' . PHP_EOL);
 	print_r($playing);
+	print_r('-- end playing --' . PHP_EOL);
 
+	print_r('-- playing2 --' . PHP_EOL);
 	print_r($playing2);
+	print_r('-- end playing2 --' . PHP_EOL);
 
+	print_r('-- recent --' . PHP_EOL);
+	print_r($recent);
+	print_r('-- end recent --' . PHP_EOL);
+
+	print_r('-- me --' . PHP_EOL);
 	print_r($me);
+	print_r('-- end me --' . PHP_EOL);
 
+	print_r('-- devices --' . PHP_EOL);
 	print_r($devices);
+	print_r('-- end devices --' . PHP_EOL);
 
